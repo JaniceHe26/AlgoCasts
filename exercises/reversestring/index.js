48e63bba22358c35f502db22f2dbedc93f3f980a
Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
2020-10-29 15:57:51 -0400

    Reversed String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //My Solution
+  // if (str.length <= 1) return str;
+
+  // let word = "";
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   word += str[i];
+  // }
+  // return word;
+
+  //Solution 1
+  // return str.split("").reverse().join("");
+
+  //Solution 2
+  // let reversed = '';
+  // for (let char of str) {
+  //   //order matters as if char the current char comes first instead of reversed + char <= that will be the same string
+  //   reversed = char + reversed;
+  // }
+  // return reversed;
+
+  //Solution 3
+  // return str.split("").reduce((reversed, character) => {
+  //   return character + reversed
+  // }, "")
+
+  debugger;
+  //node inspect index.js, c, repl
+  return str.split('').reduce((rev, char) => char + rev, '');
+
+}
+
+reverse('asdas');
 
 module.exports = reverse;
